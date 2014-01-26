7903971b146baaacc3c669710eff8e5931414555
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-01-26 10:26:02 -0600

    Integrate global command-line options with config file

--- lib/passman/config.rb
+++ lib/passman/config.rb
@@ -8,11 +8,30 @@ module Passman
     INITIAL_CONFIG = "config/passman.conf"
     DEFAULT_CONFIG = "~/.config/passman.conf"
 
-    def initialize(global_options, path = DEFAULT_CONFIG)
+    def initialize(cli_options, path = DEFAULT_CONFIG)
       @path = File.expand_path path
 
       write_config unless File.exists? @path
       @config = TOML.load_file(@path)
+
+      cli_config = cli_options_to_hash(cli_options)
+      @config.merge!(cli_config) { |k, x, y| x.merge(y) }
+    end
+
+    def cli_options_to_hash(options)
+      options
+        .reject { |k, v| k.is_a? Symbol }
+        .reduce(Hash.new) do |hash, kv|
+          hash.tap do |h|
+            k, v = kv
+            k1, k2 = k.split /-/
+
+            if k1 && k2 && v
+              h[k1] ||= {}
+              h[k1][k2] = v
+            end
+          end
+      end
     end
 
     def [](*args)
--- passman.gemspec
+++ passman.gemspec
@@ -16,4 +16,6 @@ spec = Gem::Specification.new do |s|
   s.add_runtime_dependency('gli','2.9.0')
   s.add_runtime_dependency('gpgme', '2.0.2')
   s.add_runtime_dependency('toml', '0.1.0')
+
+  s.add_development_dependency('debugger')
 end
