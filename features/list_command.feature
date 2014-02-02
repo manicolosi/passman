19ef44c2b178106b92129f537bfb99ca5314490b
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-02-02 01:45:56 -0600

    Adds test for multiple records with list command

--- features/list_command.feature
+++ features/list_command.feature
@@ -2,10 +2,27 @@ Feature: List Command
 
   Scenario: Empty database
     Given I don't have an existing database
-    When I run 'list'
+    When I run "list"
     Then I see no output
 
   Scenario: One record
-    Given I have created a record
-    When I run 'list'
-    Then I see 'mycategory/myidentifier'
+    Given I have created a record:
+      | myidentifier | mycategory |
+    When I run "list"
+    Then I see:
+      """
+      mycategory/myidentifier
+      """
+
+  Scenario: Multiple records
+    Given I have created a record:
+      | github.com   | dev      |
+      | gmail.com    | personal |
+      | facebook.com | social   |
+    When I run "list"
+    Then I see:
+      """
+      dev/github.com
+      personal/gmail.com
+      social/facebook.com
+      """
--- features/step_definitions/list_command_steps.rb
+++ features/step_definitions/list_command_steps.rb
@@ -1,43 +1,41 @@
 require 'open3'
 
 class Command
-  attr_reader :command_line, :stdout, :stderr, :status
+  attr_reader :command_line, :stdout, :stderr
 
   def initialize(command, *args)
     @command_line = ['bundle exec passman', command, *args].flatten.join(' ')
   end
 
   def execute
-    Open3.popen3 @command_line do |stdin, stdout, stderr, wait_thr|
-      @stdout = stdout.gets
-      @stderr = stderr.gets
-      @status = wait_thr.value.exitstatus
-    end
+    stdout, stderr, _ = Open3.capture3 @command_line
+    @stdout = stdout.to_s
+    @stderr = stderr.to_s
 
     self
   end
-end
-
-def assert_stdout(expected)
-  expect(@command.stdout.to_s.chomp).to eq(expected.chomp)
 end
 
 Given(/^I don't have an existing database$/) do
   # We start with no database
 end
 
-Given(/^I have created a record$/) do
-  @command = Command.new('new', 'identifier=myidentifier', 'category=mycategory').execute
+Given(/^I have created a record:$/) do |table|
+  table.raw.each do |identifier, category|
+    identifier = "identifier=#{identifier}"
+    category   = "category=#{category}"
+    @command = Command.new('new', identifier, category).execute
+  end
 end
 
-When(/^I run '(.+)'$/) do |cmd|
+When(/^I run "(.+)"$/) do |cmd|
   @command = Command.new(cmd).execute
 end
 
 Then(/^I see no output$/) do
-  assert_stdout ''
+  expect(@command.stdout).to eq('')
 end
 
-Then(/^I see '(.+)'$/) do |expected_output|
-  assert_stdout expected_output
+Then(/^I see:$/) do |expected|
+  expect(@command.stdout.chomp).to eq(expected)
 end
