7f7c57d1a928464940967d25ce5b2b87eaf663b5
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-02-01 22:08:04 -0600

    Integration test for listing with a record

--- features/list_command.feature
+++ features/list_command.feature
@@ -1,9 +1,11 @@
 Feature: List Command
-  In order to see what records exist
-  As a user
-  I want to see a quick list of my records
 
   Scenario: Empty database
     Given I don't have an existing database
     When I run 'list'
     Then I see no output
+
+  Scenario: One record
+    Given I have created a record
+    When I run 'list'
+    Then I see 'mycategory/myidentifier'
--- features/step_definitions/list_command_steps.rb
+++ features/step_definitions/list_command_steps.rb
@@ -1,10 +1,36 @@
+require 'open3'
+
+def execute(command, *args)
+  exec = ['bundle exec passman', command, *args].flatten.join(' ')
+
+  Open3.popen3 exec do |stdin, stdout, stderr, wait_thr|
+    @stdout = stdout.gets
+    @stderr = stderr.gets
+    @exit_status = wait_thr.value.exitstatus
+  end
+end
+
+def assert_stdout(expected)
+  expect(@stdout.to_s.chomp).to eq(expected.chomp)
+end
+
+
 Given(/^I don't have an existing database$/) do
+  # We start with no database
+end
+
+Given(/^I have created a record$/) do
+  execute 'new', 'identifier=myidentifier', 'category=mycategory'
 end
 
 When(/^I run '(.+)'$/) do |cmd|
-  @output = `bundle exec passman #{cmd}`
+  execute cmd
 end
 
 Then(/^I see no output$/) do
-  expect(@output).to eq('')
+  assert_stdout ''
+end
+
+Then(/^I see '(.+)'$/) do |expected_output|
+  assert_stdout expected_output
 end
