96dbed03a503ef2d18d4779b85f29360f2a30655
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-02-02 11:28:20 -0600

    Move hooks into a separate file

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,45 +1,7 @@
 require_relative 'command_helper'
+require_relative 'hooks'
 
 World(CommandHelper)
-
-require 'tmpdir'
-require 'fileutils'
-require 'open3'
-
-module Hooks
-  def self.global_setup
-    print 'Setting up environment... '
-
-    ENV.delete 'GPG_AGENT_INFO'
-
-    @home = Dir.mktmpdir
-    ENV['HOME'] = @home
-
-    param_file = File.join(File.dirname(__FILE__), 'gpg-params.txt')
-    cmd = "gpg --debug-quick-random --batch --gen-key #{param_file}"
-    Open3.popen3 cmd do |stdin, stdout, stderr, wait_thr|
-      raise "Failed to generate GPG keys" unless wait_thr.value.success?
-    end
-
-    puts 'done.'
-  end
-
-  def self.global_teardown
-    recursive_delete(@home)
-  end
-
-  def self.teardown
-    ['.config', '.local'].each do |dir|
-      recursive_delete(@home, dir)
-    end
-  end
-
-  def self.recursive_delete(*components)
-    dir = File.join(*components)
-
-    FileUtils.rm_r dir if File.directory? dir
-  end
-end
 
 at_exit do
   Hooks.global_teardown
---
+++ features/support/hooks.rb
@@ -0,0 +1,38 @@
+require 'tmpdir'
+require 'fileutils'
+require 'open3'
+
+module Hooks
+  def self.global_setup
+    print 'Setting up environment... '
+
+    ENV.delete 'GPG_AGENT_INFO'
+
+    @home = Dir.mktmpdir
+    ENV['HOME'] = @home
+
+    param_file = File.join(File.dirname(__FILE__), 'gpg-params.txt')
+    cmd = "gpg --debug-quick-random --batch --gen-key #{param_file}"
+    Open3.popen3 cmd do |stdin, stdout, stderr, wait_thr|
+      raise "Failed to generate GPG keys" unless wait_thr.value.success?
+    end
+
+    puts 'done.'
+  end
+
+  def self.global_teardown
+    recursive_delete(@home)
+  end
+
+  def self.teardown
+    ['.config', '.local'].each do |dir|
+      recursive_delete(@home, dir)
+    end
+  end
+
+  def self.recursive_delete(*components)
+    dir = File.join(*components)
+
+    FileUtils.rm_r dir if File.directory? dir
+  end
+end
