bd69cb0c2873172d4eb1c54db362fbf5a991740a
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-02-02 12:23:26 -0600

    Reordering step definitions

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -2,6 +2,9 @@ Given(/^I don't have an existing database$/) do
   # We start with no database
 end
 
+Given(/^I don't have a configuration file$/) do
+end
+
 Given(/^I have created (?:this|these) record(?:s?):$/) do |table|
   table.raw.each do |identifier, category|
     identifier = "identifier=#{identifier}"
@@ -20,9 +23,6 @@ end
 
 Then(/^I see:$/) do |expected|
   stdout.chomp.should == expected
-end
-
-Given(/^I don't have a configuration file$/) do
 end
 
 Then(/^I see text like "(.*?)" on stderr$/) do |text|
