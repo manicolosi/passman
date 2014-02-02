313146576010cd3f058d28c2a2356f970ac6a3fe
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-02-02 01:17:16 -0600

    Enable quick key generation for gpg

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,16 +4,20 @@ require 'open3'
 
 module Hooks
   def self.global_setup
+    print 'Setting up environment... '
+
     ENV.delete 'GPG_AGENT_INFO'
 
     @home = Dir.mktmpdir
     ENV['HOME'] = @home
 
     param_file = File.join(File.dirname(__FILE__), 'gpg-params.txt')
-    cmd = "gpg --batch --gen-key #{param_file}"
+    cmd = "gpg --debug-quick-random --batch --gen-key #{param_file}"
     Open3.popen3 cmd do |stdin, stdout, stderr, wait_thr|
-      raise "Failed to generate gpg keys" unless wait_thr.value.success?
+      raise "Failed to generate GPG keys" unless wait_thr.value.success?
     end
+
+    puts 'done.'
   end
 
   def self.global_teardown
