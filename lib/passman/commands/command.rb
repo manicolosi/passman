8a31d452365db5dfe781b0468b146d1993edae9e
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-02-02 19:26:53 -0600

    Include Commands module outside the Command class

--- lib/passman/commands/command.rb
+++ lib/passman/commands/command.rb
@@ -1,42 +1,44 @@
 require_relative '../helpers/hyphenizer'
 
 module Passman
-  class Command
-    attr_reader :global, :options, :args
-    attr_reader :config, :database
+  module Commands
+    class Command
+      attr_reader :global, :options, :args
+      attr_reader :config, :database
 
-    def initialize(global, options, args)
-      @global = global
-      @options = options
-      @args = args
+      def initialize(global, options, args)
+        @global = global
+        @options = options
+        @args = args
 
-      @config = global[:config]
-      @database = global[:database]
-    end
+        @config = global[:config]
+        @database = global[:database]
+      end
 
-    singleton_class.send(:alias_method, :original_name, :name)
+      singleton_class.send(:alias_method, :original_name, :name)
 
-    def self.name
-      Helpers::Hyphenizer.hyphenize(original_name)
-    end
+      def self.name
+        Helpers::Hyphenizer.hyphenize(original_name)
+      end
 
-    def self.desc(*arg)
-      getter_setter :desc, arg
-    end
+      def self.desc(*arg)
+        getter_setter :desc, arg
+      end
 
-    def self.arg_name(*arg)
-      getter_setter :arg_name, arg
-    end
+      def self.arg_name(*arg)
+        getter_setter :arg_name, arg
+      end
 
-    def self.invoke(global, options, args)
-      new(global, options, args).invoke
-    end
+      def self.invoke(global, options, args)
+        new(global, options, args).invoke
+      end
 
-    def self.getter_setter(var, arg)
-      if arg.empty?
-        instance_variable_get "@#{var}"
-      else
-        instance_variable_set "@#{var}", arg.first
+      def self.getter_setter(var, arg)
+        if arg.empty?
+          instance_variable_get "@#{var}"
+        else
+          instance_variable_set "@#{var}", arg.first
+        end
       end
     end
   end
