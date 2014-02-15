f6500f86cf32e731b75f1253d1a4f3c131b01021
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-02-14 21:31:05 -0600

    Refactor how commands are required

--- lib/passman/commands.rb
+++ lib/passman/commands.rb
@@ -1,11 +1,7 @@
 module Passman
   module Commands
-    Dir[ File.join(File.dirname(__FILE__), 'commands/*.rb') ].each do |file|
-      require file
-    end
-
     def self.extended(base)
-      commands.each do |cmd|
+      all.each do |cmd|
         base.desc cmd.desc
         base.arg_name cmd.arg_name
         base.command cmd.name do |c|
@@ -17,8 +13,16 @@ module Passman
       end
     end
 
-    def self.commands
+    def self.all
       constants.map { |c| const_get(c) }.reject { |c| c == Command }
+    end
+
+    def self.files
+      Dir[ File.join(File.dirname(__FILE__), 'commands/*.rb') ]
+    end
+
+    files.each do |file|
+      require file
     end
   end
 end
