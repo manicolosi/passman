35107a41220215461b3b463e1aca986a9f172f2c
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-02-20 22:01:36 -0600

    Refactor copy command

--- lib/passman/commands/copy.rb
+++ lib/passman/commands/copy.rb
@@ -10,12 +10,17 @@ module Passman
         password = database.find_one(args.first).password
         raise "Record does not have a password" unless password
 
-        read_cmd = config['commands', 'read_clipboard']
-        write_cmd = config['commands', 'write_clipboard']
+        clipboard = `#{read_clipboard}`
+        system "echo -n '#{password}' | #{write_clipboard}"
+        system "sleep 5 && echo -n | echo -n '#{clipboard}' | #{write_clipboard} &"
+      end
 
-        clipboard = `#{read_cmd}`
-        system "echo -n '#{password}' | #{write_cmd}"
-        system "sleep 5 && echo -n | echo -n '#{clipboard}' | #{write_cmd} &"
+      def read_clipboard
+        config['commands', 'read_clipboard']
+      end
+
+      def write_clipboard
+        config['commands', 'write_clipboard']
       end
     end
   end
